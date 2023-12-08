34fd13311df82864cd261d1828b3354dd9c6be20
Serge3452 <127433233+Serge3452@users.noreply.github.com>
2023-12-08 13:49:42 +0500

    Урок и ДЗ №2

---
+++ lesson3.py
@@ -0,0 +1,73 @@
+#Домашнее заданее урок 3
+# a=20
+# b=int (input('Noner'))
+# c=100
+# d=200
+# if a==b:
+#    print('1')
+# elif a>b:
+#     print('2')
+# else:
+#     print("4")
+
+# number=int (input(Число))
+# if input>0
+#     print ("Положительное")
+# elif: input ==0
+# else:
+#     print )
+
+# # 1
+# a=int(input('A'))
+# b=int(input('B'))
+# if a<b:
+#     print ('a<b')
+# elif a>b:
+#     print ("a>b")
+# else:
+#     print('a=b')
+
+# scope = int(input('Ваш балл:'))
+# if scope >= 90:
+#     print ('Отлично')
+# elif scope >= 70:
+#     print ('Хорошо')
+# elif scope >= 50:
+#     print ('Удовл')
+# else:
+#     print  ('Неуд')
+
+
+# ДЗ №1
+# season = int(input('Введите месяц годв в виде числа :'))
+# if season > 12 or season <=0:
+#     print ('Вы ошиблись, введите цифру с 1 до 12')
+# elif season == 12:
+#     print ('Зима')
+# elif season >= 9:
+#     print ('Осень')
+# elif season >= 6:
+#     print ('Лето')
+# elif season >= 3:
+#     print ('Весна')    
+# else:
+#     print ('Зима')
+
+
+# #ДЗ №2 ( сравнение чисел)
+# a=11
+# b = int(input('Введите число :'))
+# if b-a > 100:
+#     print ('число больше 100')
+# elif b-a > 50:
+#     print ('число больше 50')
+# else:
+#     print ('число меньше 50')18
+
+#ДЗ №2 ( опреденение возраста)
+
+age = int(input('Введите ваш возрост :'))
+if age >= 18:
+    print ('Доступ к ресурсу разрешен!')
+else:
+    print ('Здесь ограничение 18+, я не то что детям, сам смотреть боюсь :)))')
